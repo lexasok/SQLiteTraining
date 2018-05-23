4d79a6cc7d4bc4ff8832a621efd8951208f0a942
lexa <sokolov@ozero.net>
2018-05-23 10:44:21 +0300

    Fields added

--- app/src/main/java/net/ozero/sqlitetraining/MainActivity.java
+++ app/src/main/java/net/ozero/sqlitetraining/MainActivity.java
@@ -2,12 +2,23 @@ package net.ozero.sqlitetraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAdd;
+    private Button buttonRead;
+    private Button buttonClear;
+
+    private EditText editTextName;
+    private EditText editTextEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
