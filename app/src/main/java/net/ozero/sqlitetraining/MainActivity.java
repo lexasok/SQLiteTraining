33a54c2b3c0bc21561c956026fa58e765e479eb7
lexa <sokolov@ozero.net>
2018-05-23 10:53:13 +0300

    Views initialized, onClickListener set.

--- app/src/main/java/net/ozero/sqlitetraining/MainActivity.java
+++ app/src/main/java/net/ozero/sqlitetraining/MainActivity.java
@@ -2,10 +2,11 @@ package net.ozero.sqlitetraining;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonAdd;
     private Button buttonRead;
@@ -18,6 +19,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(this);
+
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonClear.setOnClickListener(this);
+
+        buttonRead = findViewById(R.id.buttonRead);
+        buttonRead.setOnClickListener(this);
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
 
 
     }
