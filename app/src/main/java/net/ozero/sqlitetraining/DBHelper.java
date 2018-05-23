c860ea86f092d9d7765a675f18e2356a6855b237
lexa <sokolov@ozero.net>
2018-05-23 11:09:39 +0300

    Constants added

--- app/src/main/java/net/ozero/sqlitetraining/DBHelper.java
+++ app/src/main/java/net/ozero/sqlitetraining/DBHelper.java
@@ -5,8 +5,18 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactDb";
+    private static final String TABLE_CONTACTS = "contacts";
+
+    private static final String KEY_ID = "_id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_EMAIL = "email";
+
+
+    public DBHelper(Context context, String name, int version) {
+        super(context, name, null, version);
     }
 
     @Override
